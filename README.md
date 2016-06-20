b41ef1919a0704b9b8b68f22fd5bd5baa3c3a52d
lex <alex@bighill.org>
2016-06-20 15:40:11 -0700

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# common desktop scripts and configs #
+
+helpful on development machines using ubuntu (and other linux)
---
+++ mouse-sensitivity.sh
@@ -0,0 +1,5 @@
+# set mouse sensitivity (or movement speed)
+#
+# 9 = input id. found using this command: $ xinput --list --short
+# 2 = sensitivity (lower is fast/unweildy, higher is accurate/slow)
+xinput --set-prop 9 "Device Accel Constant Deceleration" 2
